8c51928d7be6474996dd7ffc902a5d3d3453281c
kyokomi <kyoko1220adword@gmail.com>
2014-09-19 10:02:02 +0900

    fix tutorial 4

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/logout", doLogoutHandler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +27,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprint(w, "Hello, %v!", u)
+}
+
+func doLogoutHandler(w http.ResponseWriter, r *http.Request) {
+
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u != nil {
+		url, err := user.LogoutURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	http.Redirect(w, r, "/", 200)
 }
