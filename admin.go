0ca6212e8028f6b32d3ad220c619abc14a944940
kyokomi <kyoko1220adword@gmail.com>
2014-10-11 19:54:48 +0900

    fix(admin): appengine/runtime

---
+++ admin.go
@@ -0,0 +1,37 @@
+package gaehoge
+
+import (
+	"net/http"
+	"appengine"
+	"appengine/runtime"
+	"fmt"
+)
+
+func init() {
+	http.HandleFunc("/admin/show-runtime", doShowRuntime)
+}
+
+// appengin.runtime example
+func doShowRuntime(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	stats, err := runtime.Stats(c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.Header().Set("Content-Type", "text/plain")
+		// インスタンスのCPU使用量
+		fmt.Fprintf(w, "CPU Total: %f\n", stats.CPU.Total)
+		// 1分あたりのCPU使用率
+		fmt.Fprintf(w, "CPU Rate1M: %f\n", stats.CPU.Rate1M)
+		// 10分あたりのCPU使用率
+		fmt.Fprintf(w, "CPU Rate10M: %f\n", stats.CPU.Rate10M)
+
+		// メモリの使用量
+		fmt.Fprintf(w, "RAM Current: %f\n", stats.RAM.Current)
+		// 1分あたりの平均メモリ使用量
+		fmt.Fprintf(w, "RAM Average1M: %f\n", stats.RAM.Average1M)
+		// 10分あたりの平均メモリ使用量
+		fmt.Fprintf(w, "RAM Average10M: %f\n", stats.RAM.Average10M)
+	}
+}
