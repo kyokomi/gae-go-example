97abc9dd46fc439ac95e45b7285d57dea221a9ea
kyokomi <kyoko1220adword@gmail.com>
2014-10-12 12:31:51 +0900

    test: added 3 cases TestSign

--- main_test.go
+++ main_test.go
@@ -13,6 +13,12 @@ import (
 
 	"appengine/aetest"
 	"app/controllers"
+	"appengine/user"
+	"app/models"
+	"appengine"
+	"appengine/datastore"
+	"time"
+	"reflect"
 )
 
 type handlerTest struct {
@@ -53,6 +59,7 @@ func TestIndex(t *testing.T) {
 }
 
 func TestSign(t *testing.T) {
+	now := time.Now()
 
 	inst, err := aetest.NewInstance(nil)
 	if err != nil {
@@ -60,22 +67,84 @@ func TestSign(t *testing.T) {
 	}
 	defer inst.Close()
 
-	val := url.Values{}
-	val.Set("content", "hogehoge")
-	req, err := inst.NewRequest("POST", "/sign", strings.NewReader(val.Encode()))
-	if err != nil {
-		t.Error(err)
+	testCases := []struct {
+		method   string
+		content  string
+		user     *user.User
+		code     int
+		greeting *models.Greeting
+	}{
+		{
+			method: "GET",
+			code:   405,
+		},
+		{
+			method:   "POST",
+			content:  "Normal post",
+			code:     303,
+			greeting: &models.Greeting{Content: "Normal post"},
+		},
+		{
+			method:   "POST",
+			content:  "Post with user",
+			user:     &user.User{Email: "hoge@gmail.com"},
+			code:     303,
+			greeting: &models.Greeting{Content: "Post with user", Author: "hoge@gmail.com"},
+		},
 	}
 
-	res := httptest.NewRecorder()
+	for _, tt := range testCases {
+		// create RequestData
+		val := url.Values{
+			"content": []string{tt.content},
+		}
 
-	controllers.GuestSign(res, req)
+		req, err := inst.NewRequest(tt.method, "/guest-book/sign", strings.NewReader(val.Encode()))
+		if err != nil {
+			t.Error(err)
+		}
 
-	if res.Header().Get("Location") != "/" {
-		t.Error("bad response location URL ", res.Header().Get("Location"))
-	}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		if tt.user != nil {
+			aetest.Login(tt.user, req)
+		}
 
-	if http.StatusFound != res.Code {
-		t.Error("bad response status code ", res.Code)
+		resp := httptest.NewRecorder()
+		// exec request
+		controllers.GuestSign(resp, req)
+
+		if resp.Code != tt.code {
+			t.Errorf("Got response code %d; want %d; body:\n%s", resp.Code, tt.code, resp.Body.String())
+		}
+
+		// Check the latest greeting against our expectation.
+		c := appengine.NewContext(req)
+		q := datastore.NewQuery("Greeting").Ancestor(models.GuestBookKey(c)).Order("-Date").Limit(1)
+		var g models.Greeting
+		_, err = q.Run(c).Next(&g)
+		if err == datastore.Done {
+			if tt.greeting != nil {
+				t.Errorf("No greeting stored. Expected %v", tt.greeting)
+			}
+			continue
+		}
+		if err != nil {
+			t.Errorf("Failed to fetch greeting: %v", err)
+		}
+		if tt.greeting == nil {
+			if !g.Date.Before(now) {
+				t.Errorf("Expected no new greeting, found: %v", g)
+			}
+			continue
+		}
+
+		if g.Date.Before(now) {
+			t.Errorf("Greeting stored at %v, want at least %v", g.Date, now)
+		}
+
+		g.Date = time.Time{}
+		if !reflect.DeepEqual(g, *tt.greeting) {
+			t.Errorf("Greetings don't match. \nGot: %v\nWant: %v", g, *tt.greeting)
+		}
 	}
 }
